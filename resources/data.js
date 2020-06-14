2617b8f6736fd6d6a0185afb8eeeea5ef4ce2aad
Markus Mannheim <markus@mannheim.media>
2020-06-14 15:03:33 +1000

    Add files via upload

--- resources/data.js
+++ resources/data.js
@@ -8,7 +8,8 @@ fs.readFile("sa3s.geojson", "utf8", function(error, data) {
   fs.readFile("deaths.csv", "utf8", function(error, stats) {
     if (error) throw error;
     statistics = d3.csvParse(stats)
-      .filter(function(d) { return d.Year == "2015-2017" && +d.Rate; });
+      // .filter(function(d) { return d.Year == "2015-2017" && +d.Rate; });
+      .filter(function(d) { return d.Year == "2015-2017" && +d["SA3 code"].slice(0, 1) < 9; });
 
     regions = new Set();
     statistics.forEach(function(d) {
@@ -48,9 +49,9 @@ fs.readFile("sa3s.geojson", "utf8", function(error, data) {
           .filter(function(e) {
             return e.Sex == "Persons";
           });
-        if (males.length > 0) region.properties.males = +males[0].Rate;
-        if (females.length > 0) region.properties.females = +females[0].Rate;
-        if (people.length > 0) region.properties.people = +people[0].Rate;
+        if (males.length > 0) region.properties.males = males[0].Rate == "NP" ? "na" : +males[0].Rate;
+        if (females.length > 0) region.properties.females = females[0].Rate == "NP" ? "na" : +females[0].Rate;
+        if (people.length > 0) region.properties.people = people[0].Rate == "NP" ? "na" : +people[0].Rate;
         newData.push(region);
       }
     });
